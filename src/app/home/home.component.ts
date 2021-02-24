cb776b50bb02f1df57063da0c9f7abb2a3700007
arada88 <arada88@hotmail.com>
2021-02-24 13:43:46 +0700

    lab4

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,9 +10,10 @@ export class HomeComponent implements OnInit {
    @Input()
    title: string;
 
-   
+
   constructor() { }
-   
+
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   count = 0;
   countNumber() {
     this.count = this.count + 1;
