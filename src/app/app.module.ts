849fca66df49cb5589a9f01821ce963be770b5b4
arada88 <arada88@hotmail.com>
2021-02-24 14:11:36 +0700

    homework1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { CountingComponent } from './counting/counting.component';
 import { Lab4Component } from './lab4/lab4.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TopbannerComponent } from './topbanner/topbanner.component';
+import { CategorymenuComponent } from './categorymenu/categorymenu.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CountingComponent,
-    Lab4Component
+    Lab4Component,
+    NavbarComponent,
+    TopbannerComponent,
+    CategorymenuComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
