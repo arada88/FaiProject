4c570fd549ebe4dbe0df4872706a5a223926456b
arada88 <arada88@hotmail.com>
2021-03-05 15:48:05 +0700

    hw3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { TopContentComponent } from './top-content/top-content.component';
+import { SearchComponent } from './search/search.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderMenuComponent,
+    FooterComponent,
+    TopContentComponent,
+    SearchComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
