0708b7e45e8c4692ed5f96529aace2624028307d
arada88 <arada88@hotmail.com>
2021-03-07 20:00:24 +0700

    ep24new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {BackendService } from './backend.service';
+import { CatalogModule } from './catalog/catalog.module';
+import { HomeModule } from './home/home.module';
+//import { ProductListComponent} from './product-list/product-list.component';
+//import { ProductItemComponent } from './product-item/product-item.component';
+//import { PanelComponent } from './panel/panel.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, CatalogModule, HomeModule
   ],
-  providers: [],
+  providers: [BackendService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
